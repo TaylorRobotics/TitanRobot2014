a09da5ff4878993eb54b54427deb3b65acd12cb7
RollieC <rolliecopp@gmail.com>
2014-02-03 17:25:02 -0500

    Added scaling option to Potentiometer and added PotentiometerLimitSwitch

--- src/us/in/k12/taylor/robotics/robot2014/components/Potentiometer.java
+++ src/us/in/k12/taylor/robotics/robot2014/components/Potentiometer.java
@@ -9,13 +9,45 @@ import us.in.k12.taylor.robotics.robot2014.RobotParameters;
 public class Potentiometer implements RobotParameters {
     private final AnalogChannel analogVoltageMeter;
     private final AnalogChannel potentiometer;
+    private final boolean scaled;
+    private final double scale;
+    private final double minValue;
+    private final double maxValue;
 
     public Potentiometer(int pPotentiometerChannel, AnalogChannel pAnalogVoltageMeter) {
         potentiometer = new AnalogChannel(pPotentiometerChannel);
         analogVoltageMeter = pAnalogVoltageMeter;
+        scaled = false;
+        scale = 0.0;
+        minValue = 0.0;
+        maxValue = 0.0;
+    }
+
+    public Potentiometer(int pPotentiometerChannel, AnalogChannel pAnalogVoltageMeter, double pScale, double pMinValue, double pMaxValue) {
+        potentiometer = new AnalogChannel(pPotentiometerChannel);
+        analogVoltageMeter = pAnalogVoltageMeter;
+        scaled = true;
+        scale = pScale;
+        minValue = pMinValue;
+        maxValue = pMaxValue;
     }
 
     public double getRatio() {
-        return potentiometer.getVoltage() / analogVoltageMeter.getVoltage();
+        return potentiometer.getVoltage() / analogVoltageMeter.getVoltage();       
+    }
+
+    public double getValue() {
+        double value = getRatio();
+        if (scaled) {
+            value = (maxValue-minValue)/(value-minValue);
+            if (value < 0.0) {
+                value = 0.0;
+            }
+            else if (value > 1.0) {
+                value = 1.0;
+            }
+            value = value * scale;
+        }
+        return value;
     }
 }
---
+++ src/us/in/k12/taylor/robotics/robot2014/components/PotentiometerLimitSwitch.java
@@ -0,0 +1,29 @@
+package us.in.k12.taylor.robotics.robot2014.components;
+
+/**
+ * @author Taylor Robotics 2014
+ */
+public class PotentiometerLimitSwitch extends Switch {
+    private final Potentiometer potentiometer;
+    private final boolean lowLimit;
+    private final double limitValue;
+
+    public PotentiometerLimitSwitch(Potentiometer pPotentiometer, boolean pLowLimit, double pLimitValue, boolean pForceStateChange) {
+        super(pForceStateChange);
+        potentiometer = pPotentiometer;
+        lowLimit = pLowLimit;
+        limitValue = pLimitValue;
+    }
+    
+    public boolean getSwitchState() {
+        boolean switchOn;
+        double value = potentiometer.getValue();
+        if (lowLimit) {
+            switchOn = (value <= limitValue);
+        }
+        else {
+            switchOn = (value >= limitValue);
+        }
+        return switchOn;
+    }
+}
