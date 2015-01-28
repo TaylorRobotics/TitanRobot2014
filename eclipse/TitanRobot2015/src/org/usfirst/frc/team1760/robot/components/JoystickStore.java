a202677f75b603915f6cb990e4af393930f41cb7
RollieC <rolliecopp@gmail.com>
2015-01-27 22:07:52 -0500

    Remove parking and holding components

--- eclipse/TitanRobot2015/src/org/usfirst/frc/team1760/robot/components/JoystickStore.java
+++ eclipse/TitanRobot2015/src/org/usfirst/frc/team1760/robot/components/JoystickStore.java
@@ -13,10 +13,6 @@ public class JoystickStore {
     public static final int SLOW_SPEED_BUTTON = 1; // On left driver joystick
     public static final int FAST_SPEED_BUTTON = 1; // On right driver joystick
 
-    public static final int HOLD_ELEVATOR_HIGH_BUTTON = 6;   // On operator joystick
-    public static final int PARK_ELEVATOR_BUTTON = 7;    // On operator joystick
-    public static final int CANCEL_ELEVATOR_HOLD_BUTTON = 8; // On operator joystick
-
     private Joystick leftDriveJoystick = null;
     private Joystick rightDriveJoystick = null;
     private Joystick operatorJoystick = null;
@@ -24,9 +20,6 @@ public class JoystickStore {
     private JoystickButton liftUpButton = null;
 	private JoystickButton slowSpeedButton = null;
 	private JoystickButton fastSpeedButton = null;
-	private JoystickButton holdElevatorHighButton = null;
-	private JoystickButton parkElevatorButton = null;
-	private JoystickButton cancelElevatorHoldButton = null;
 
     public synchronized Joystick getLeftDriveJoystick() {
     	if (leftDriveJoystick == null) {
@@ -75,27 +68,6 @@ public class JoystickStore {
 			fastSpeedButton = new JoystickButton(rightDriveJoystick, FAST_SPEED_BUTTON, false);
 		}
 		return fastSpeedButton;
-	}
-
-	public synchronized JoystickButton getParkElevatorButton() {
-		if (parkElevatorButton == null) {
-			parkElevatorButton = new JoystickButton(operatorJoystick, PARK_ELEVATOR_BUTTON, false);
-		}
-		return parkElevatorButton;
-	}
-
-	public synchronized JoystickButton getHoldElevatorHighButton() {
-		if (holdElevatorHighButton == null) {
-			holdElevatorHighButton = new JoystickButton(operatorJoystick, HOLD_ELEVATOR_HIGH_BUTTON, false);
-		}
-		return holdElevatorHighButton;
-	}
-
-	public synchronized JoystickButton getCancelElevatorHoldButton() {
-		if (cancelElevatorHoldButton == null) {
-			cancelElevatorHoldButton = new JoystickButton(operatorJoystick, CANCEL_ELEVATOR_HOLD_BUTTON, false);
-		}
-		return cancelElevatorHoldButton;
 	}
 
 }
