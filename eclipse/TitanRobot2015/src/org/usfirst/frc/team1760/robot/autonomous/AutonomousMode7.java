821c60ac1af1b99375d94abb8915db65f8906fd3
RollieC <rolliecopp@gmail.com>
2015-01-31 14:06:51 -0500

    Added logic for Autonomous mode 7

--- eclipse/TitanRobot2015/src/org/usfirst/frc/team1760/robot/autonomous/AutonomousMode7.java
+++ eclipse/TitanRobot2015/src/org/usfirst/frc/team1760/robot/autonomous/AutonomousMode7.java
@@ -1,22 +1,36 @@
 package org.usfirst.frc.team1760.robot.autonomous;
 
 import org.usfirst.frc.team1760.robot.TitanRobot;
+import org.usfirst.frc.team1760.robot.components.TimeLimit;
+
+import edu.wpi.first.wpilibj.RobotDrive;
 
 /**
  * This class handles Autonomous mode 7 operations.
- * Autonomous Mode 7 will drive forward 6 ft into auto zone.
+ * Autonomous Mode 7 will drive forward 6 feet into auto zone.
  * 
  * @author Robo-Titans Team 1760 Taylor High School 2015
  */
 public class AutonomousMode7 extends AutonomousMode {
+	public static final long DRIVE_TIME = 3000; // 3 Seconds
+	public static final double DRIVE_SPEED = 0.75;
+	private RobotDrive robotDrive;
+	private TimeLimit driveTime;
 
 	public AutonomousMode7(TitanRobot pRobot) {
 		super(pRobot);
+		robotDrive = robot.getMotorStore().getRobotDrive(true);
+		driveTime = new TimeLimit(DRIVE_TIME);
 	}
 
 	@Override
 	public void periodic() {
-		// Nothing to do in Dead Mode
+		if (driveTime.isTimeLimitReached()) {
+			robotDrive.drive(0.0, 0.0);
+		}
+		else {
+			robotDrive.drive(DRIVE_SPEED, 0.0);
+		}
 	}
 
 }
