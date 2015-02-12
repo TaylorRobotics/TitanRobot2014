04e5cbe71eea257063a68d7103273e4bf23ad4dd
RollieC <rolliecopp@gmail.com>
2015-02-11 21:06:48 -0500

    Start of servo store

---
+++ eclipse/TitanRobot2015/src/org/usfirst/frc/team1760/robot/stores/ServoStore.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team1760.robot.stores;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+
+/**
+ * This class holds motor base components defined for robot operation.
+ * 
+ * @author Robo-Titans Team 1760 Taylor High School 2015
+ */
+public class ServoStore {
+	public static final int HORIZONTAL_CAMERA_SERVO_CHANNEL = 9;
+	public static final int VERTICAL_CAMERA_SERVO_CHANNEL = 10;
+
+	private RobotDrive robotDrive = null;
+
+}
