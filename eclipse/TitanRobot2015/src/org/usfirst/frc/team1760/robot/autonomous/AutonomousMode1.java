3a3c2863dbbf8450ee618286268820d1a8c4639b
RollieC <rolliecopp@gmail.com>
2015-02-12 22:24:41 -0500

    Added fork lift to AutonomousMode1

--- eclipse/TitanRobot2015/src/org/usfirst/frc/team1760/robot/autonomous/AutonomousMode1.java
+++ eclipse/TitanRobot2015/src/org/usfirst/frc/team1760/robot/autonomous/AutonomousMode1.java
@@ -1,10 +1,12 @@
 package org.usfirst.frc.team1760.robot.autonomous;
 
 import org.usfirst.frc.team1760.robot.TitanRobot;
+import org.usfirst.frc.team1760.robot.components.Switch;
 import org.usfirst.frc.team1760.robot.components.TimeLimit;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
 
 /**
  * This class handles Autonomous mode 1 operations.
@@ -20,24 +22,27 @@ public class AutonomousMode1 extends AutonomousMode {
 	public static final int DROPPING_DRAGON_TAIL = 1;
 	public static final int RAISING_DRAGON_TAIL = 2;
 	public static final int DRIVING_FORWARD = 3;
-	public static final int COMPLETE = 4;
+	public static final int RAISING_FORK_LIFT = 4;
+	public static final int COMPLETE = 5;
 
 	public static final long DRIVE_BACKWARD_TIME = 500;
 	public static final long DRAGON_TAIL_DROP_TIME = 3000;
 	public static final long DRIVE_FORWARD_TIME = 1300;
 	public static final long DRAGON_TAIL_RAISE_TIME = 5000;
+	public static final long FORK_LIFT_RAISE_TIME = 500;
 
 	public static final double DRIVE_BACKWARD_SPEED = -0.50;
 	public static final double DRIVE_FORWARD_SPEED = 1.00;
-
-	private TimeLimit driveBackwardTimeLimit;
-	private TimeLimit dragonTailDropTimeLimit;
-	private TimeLimit dragonTailRaiseTimeLimit;
-	private TimeLimit driveForwardTimeLimit;
+	public static final double FORK_LIFT_SPEED = 0.5;
 
 	private DoubleSolenoid dragonTailSolenoid;
 	private DoubleSolenoid tailLiftSolenoid;
+	private SpeedController forkLiftMotor;
+	private Switch forkLiftUpperLimitSwitch;
+	RobotDrive robotDrive;
+
 	private int mode;
+	private TimeLimit modeTimeLimit;
 	private int direction;
 
 	public AutonomousMode1(TitanRobot pRobot) {
@@ -46,10 +51,11 @@ public class AutonomousMode1 extends AutonomousMode {
 	    dragonTailSolenoid = robot.getSolenoidStore().getDragonTailSolenoid();
 	    tailLiftSolenoid = robot.getSolenoidStore().getTailLiftSolenoid();
 		mode = DRIVING_BACKWARD;
-		driveBackwardTimeLimit = new TimeLimit(DRIVE_BACKWARD_TIME);
-		dragonTailDropTimeLimit = new TimeLimit();
-		dragonTailRaiseTimeLimit = new TimeLimit();
-		driveForwardTimeLimit = new TimeLimit();
+		modeTimeLimit = new TimeLimit(DRIVE_BACKWARD_TIME);
+    	tailLiftSolenoid.set(DoubleSolenoid.Value.kForward);
+    	forkLiftMotor = robot.getMotorStore().getForkLiftMotor();
+		forkLiftUpperLimitSwitch = robot.getSwitchStore().getForkLiftUpperLimitSwitch();
+		robotDrive = robot.getMotorStore().getRobotDrive(direction == FORWARD);
 	}
 
 	/* (non-Javadoc)
@@ -57,47 +63,56 @@ public class AutonomousMode1 extends AutonomousMode {
 	 */
 	@Override
 	public void periodic() {
-		RobotDrive robotDrive = robot.getMotorStore().getRobotDrive(direction == FORWARD);
-		double speed = 0.0;
+		double driveSpeed = 0.0;
+		double forkSpeed = 0.0;
 		if (mode == DRIVING_BACKWARD) {
-			if (driveBackwardTimeLimit.isTimeLimitReached()) {
+			if (modeTimeLimit.isTimeLimitReached()) {
 				mode = DROPPING_DRAGON_TAIL;
-				dragonTailDropTimeLimit.setTimeLimit(DRAGON_TAIL_DROP_TIME);
+				modeTimeLimit.setTimeLimit(DRAGON_TAIL_DROP_TIME);
 			}
 			else {
-				speed = DRIVE_BACKWARD_SPEED;
+				driveSpeed = DRIVE_BACKWARD_SPEED;
 			}
 		}
 		if (mode == DROPPING_DRAGON_TAIL) {
         	dragonTailSolenoid.set(DoubleSolenoid.Value.kReverse);
         	tailLiftSolenoid.set(DoubleSolenoid.Value.kReverse);
-			if (dragonTailDropTimeLimit.isTimeLimitReached()) {
+			if (modeTimeLimit.isTimeLimitReached()) {
 				mode = DRIVING_FORWARD;
-				driveForwardTimeLimit.setTimeLimit(DRIVE_FORWARD_TIME);
+				modeTimeLimit.setTimeLimit(DRIVE_FORWARD_TIME);
 			}
 		}
 		if (mode == DRIVING_FORWARD) {
-			if (driveForwardTimeLimit.isTimeLimitReached()) {
-				mode = RAISING_DRAGON_TAIL;
-				dragonTailRaiseTimeLimit.setTimeLimit(DRAGON_TAIL_RAISE_TIME);
+			if (modeTimeLimit.isTimeLimitReached()) {
 	        	tailLiftSolenoid.set(DoubleSolenoid.Value.kForward);
+				mode = RAISING_DRAGON_TAIL;
+				modeTimeLimit.setTimeLimit(DRAGON_TAIL_RAISE_TIME);
 			}
 			else {
-				speed = DRIVE_FORWARD_SPEED;
+				driveSpeed = DRIVE_FORWARD_SPEED;
 			}
 		}
 		if (mode == RAISING_DRAGON_TAIL) {
-			if (dragonTailRaiseTimeLimit.isTimeLimitReached()) {
-				mode = COMPLETE;
+			if (modeTimeLimit.isTimeLimitReached()) {
 	        	tailLiftSolenoid.set(DoubleSolenoid.Value.kOff);
 	        	dragonTailSolenoid.set(DoubleSolenoid.Value.kOff);
+				mode = RAISING_FORK_LIFT;
+	        	modeTimeLimit.setTimeLimit(FORK_LIFT_RAISE_TIME);
 			}
 			else {
 	        	dragonTailSolenoid.set(DoubleSolenoid.Value.kForward);
-				
 			}
 		}
-		robotDrive.drive(speed, 0.0);
+		if (mode == RAISING_FORK_LIFT) {
+			if (modeTimeLimit.isTimeLimitReached()) {
+				mode = COMPLETE;
+			}
+			else if (!forkLiftUpperLimitSwitch.isSwitchOn()){
+				forkSpeed = FORK_LIFT_SPEED;
+			}
+		}
+		robotDrive.drive(driveSpeed, 0.0);
+		forkLiftMotor.set(forkSpeed);
 	}
 
 }
